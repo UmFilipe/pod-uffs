c91a5e1f8c0d409789473301f054aa948468e33f
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2023-09-19 14:35:19 -0300

    Adicionando bubble sort, insertion sort e selection sort

--- Trabalho 1/comparacaoMetodos.c
+++ Trabalho 1/comparacaoMetodos.c
@@ -15,10 +15,22 @@
 
 void copia(int *A, int *v, int size);
 void bubbleSort(int *A, int size);
-// void selectionSort(int *A, int size);
-// void insertionSort(int *A, int size);
-// void quickSort(int *A, int size);
+void selectionSort(int *A, int size);
+void insertionSort(int *A, int size);
+void quickSort(int *A, int size);
 
+void printArray(int n, int *ptr) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d  ", ptr[i]);
+  }
+  printf("\n");
+}
+
+void swap (int *a, int *b){
+   int temp=*(b);
+   *b=*a;
+   *a=temp;
+};
 
 int main(){
 
@@ -57,17 +69,61 @@ void copia(int *A, int *V, int size){
 }
 
 void bubbleSort(int *A, int size){
-   // implementação do Bubble
+   int i, j;   
+    for (i = 0; i < size; i++) {   
+        for (j = i + 1; j < size; j++) {   
+            if (*(A + j) < *(A + i)) {   
+                swap((A + j), (A + i));  
+            } 
+        } 
+    }   
+    printf("Vetor ordenado:");
+    printArray(size,A);
+    printf("\n");
 
 }
 
 void selectionSort(int *A, int size){
-   // implementação do Selection
+    int count = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+        int min_index = i;
+
+        //find min index
+        for (int j = i + 1; j < size; j++) {
+            if (A[j] < A[min_index]) {
+                min_index = j;
+
+            }
+        }
+
+        if (min_index != i) {
+            int temp = A[i];
+            A[i] = A[min_index];
+            A[min_index] = temp;
+        }
+    }
+
+	printf("Vetor ordenado com selection\n");
+    printArray(size, A);
 
 }
 
 void insertionSort(int *A, int size){
-   // implementação do Insertion
+   	int i,j, min, t;
+    int contador=0;
+    for (i = 1; i < size; i++) {   
+        
+        min=A[i];
+        for (j=i; j>=1 && min< A[j-1];j--){
+            A[j]=A[j-1];
+           
+        }   
+        A[j]=min;
+    }
+    printf("Vetor ordenado com insertion\n");
+    printArray(size, A);
+    printf("contador %i", contador);
 
 }
 
