efefcb54df629d07fa0a2f053f3b6ef2db136cae
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2023-11-12 23:08:06 -0300

    Adicionando heap e radix sort

--- Trabalho/comparacaoMetodos.c
+++ Trabalho/comparacaoMetodos.c
@@ -1,8 +1,8 @@
 /**
  * @file comparacaoMetodos.c
  * @author Eduardo_Pazzini_Zancanaro_e_Filipe_Medeiros_de_Almeida
- * @version 0.1
- * @date 2023-09-22
+ * @version 0.2
+ * @date 2023-11-17
  *
  * @copyright Copyright (c) 2023
  *
@@ -33,23 +33,6 @@ void copia(int *A, int *V, int size){
 	for (i = 0 ; i < size ; i++)
     	     V[i] = A[i];
 }
-
-
-// void bubbleSort(int *A, int size){
-//    int i, j;
-//    long int contador = 0;   
-//     for (i = 0; i < size; i++) {   
-//         for (j = i + 1; j < size; j++) {   
-//             if (*(A + j) < *(A + i)) {   
-//                 swap((A + j), (A + i));  
-//                 contador++;
-//             } 
-//         } 
-//     }   
-
-//     printf("Contador de trocas no bubble: %ld\n", contador);
-
-// }
 
 void bubbleSort(int *A, int size) {
     int i, j;
@@ -109,23 +92,6 @@ void insertionSort(int *A, int size) {
     printf("Contador de trocas no insertion: %ld\n", contador);
 }
 
-// void insertionSort(int *A, int size){
-//    	int i,j, min;
-//     long int contador= 0;
-//     for (i = 1; i < size; i++) {   
-//         min=A[i];
-//         for (j=i; j>=1 && min< A[j-1];j--){
-//             A[j]=A[j-1];
-//             contador++;
-//         }   
-//         A[j]=min;
-//         contador++;
-//     }
-
-//     printf("Contador de trocas no insertion: %ld\n", contador);
-
-// }
-
 int quickSortAux(int *A, int baixo, int alto, long int *contador) {
     int pivo = A[alto];
     int i = (baixo - 1);
@@ -151,10 +117,91 @@ void quickSort(int *A, int baixo, int alto, long int *contador) {
 
 }
 
-int main(){
+void maxHeapify(int *A, int size, int i, long int *contador) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && A[left] > A[largest]) {
+        largest = left;
+    }
+
+    if (right < size && A[right] > A[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(&A[i], &A[largest]);
+        (*contador)++;
+        maxHeapify(A, size, largest, contador);
+    }
+}
+
+void buildMaxHeap(int *A, int size, long int *contador) {
+    for (int i = size / 2 - 1; i >= 0; i--) {
+        maxHeapify(A, size, i, contador);
+    }
+}
+
+void heapSort(int *A, int size) {
+    long int contador = 0;
+    
+    buildMaxHeap(A, size, &contador);
+
+    for (int i = size - 1; i > 0; i--) {
+        swap(&A[0], &A[i]);
+        contador++;
+        maxHeapify(A, i, 0, &contador);
+    }
+
+    printf("Contador de trocas no heap: %ld\n", contador);
+}
+
+void countingSort(int *A, int size, int exp, long int *contador) {
+    int output[size];
+    int count[10] = {0};
+
+    for (int i = 0; i < size; i++) {
+        count[(A[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[(A[i] / exp) % 10] - 1] = A[i];
+        count[(A[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (A[i] != output[i]) {
+            A[i] = output[i];
+            (*contador)++;
+        }
+    }
+}
+
+void radixSort(int *A, int size, long int *contador) {
+    int max = A[0];
+
+    for (int i = 1; i < size; i++) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countingSort(A, size, exp, contador);
+    }
+
+    printf("Contador de trocas no radix: %ld\n", *contador);
+}
+
+void main(){
 
 	int i;
-	long int tamanhoVetor = 250000;
+	long int tamanhoVetor = 100000;
 	int *vetor = (int*)malloc(tamanhoVetor * sizeof(int));
 
     srand(time(NULL));
@@ -164,7 +211,7 @@ int main(){
     } 
 
 	printf("\nVetor original: \n");
-	printArray(tamanhoVetor, vetor);
+	// printArray(tamanhoVetor, vetor);
 
 	printf("\nVetor tamanho = %ld\n", tamanhoVetor);
 
@@ -229,13 +276,43 @@ int main(){
     printf("Quick sort demorou: %f minutos", tempoQuick);
     printf("\n");
 
+    // heap sort
+    int *heapVec = (int*)malloc(tamanhoVetor * sizeof(int));
+    copia(vetor, heapVec, tamanhoVetor);
+
+    printf("\nHeap sort: \n");
+    clock_t inicioHeap = clock();
+    heapSort(heapVec, tamanhoVetor);
+    clock_t fimHeap = clock();
+    double tempoHeap = ((double)(fimHeap - inicioHeap) / CLOCKS_PER_SEC) / 60;
+    printf("\nVetor ordenado pelo heap sort: \n");
+    // printArray(tamanhoVetor, heapVec);
+    printf("Heap sort demorou: %f minutos", tempoHeap);
+    printf("\n");
+
+    // radix sort
+    int *radixVec = (int*)malloc(tamanhoVetor * sizeof(int));
+    copia(vetor, radixVec, tamanhoVetor);
+
+    printf("\nRadix sort: \n");
+    clock_t inicioRadix = clock();
+    long int contadorRadix = 0;
+    radixSort(radixVec, tamanhoVetor, &contadorRadix);
+    clock_t fimRadix = clock();
+    double tempoRadix = ((double)(fimRadix - inicioRadix) / CLOCKS_PER_SEC) / 60;
+    printf("\nVetor ordenado pelo radix sort: \n");
+    // printArray(tamanhoVetor, radixVec);
+    printf("Radix sort demorou: %f minutos", tempoRadix);
+    printf("\n");
+
     free(vetor);
     free(bubbleVec);
     free(selectionVec);
     free(insertionVec);
     free(quickVec);
+    free(heapVec);
+    free(radixVec);
 
-	return 0;
 }
 
 
